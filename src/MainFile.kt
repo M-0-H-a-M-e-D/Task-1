c113aad187b23138341257354cb4fb4eb721a275
Mohamed <mohamedhatemzakaria@gmail.com>
2022-08-05 08:15:29 +0200

    This version 2

--- src/MainFile.kt
+++ src/MainFile.kt
@@ -1,4 +1,12 @@
+import java.util.Scanner
+
 fun main()
-{
-    println("Hello World")
+{   val input=Scanner(System.`in`)
+    println("Enter First number: ")
+    val a:Int=input.nextInt()
+    println("Enter Second number: ")
+    val b:Int=input.nextInt()
+
+    print("Sum is : ")
+    println(a+b)
 }
